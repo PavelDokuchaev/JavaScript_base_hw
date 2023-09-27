c61b5baa66e282349fbcef8a114fa12309e5d681
dokuchaev.pa <dokuchaev.pa@gmail.com>
2023-09-27 22:15:03 +0400

    sem3_ECMA_hw_1

--- sem3_ECMA/task1.js
+++ sem3_ECMA/task1.js
@@ -15,32 +15,32 @@
 // const manager = new Manager("Jane Doe", "Sales");
 // manager.displayInfo(); // "Name: John Doe, Department: Sales"
 
-// class Employee {
-//   constructor(name) {
-//     this.name = name;
-//   }
+class Employee {
+  constructor(name) {
+    this.name = name;
+  }
 
-//   displayInfo() {
-//     console.log(`Name: ${this.name}`);
-//   }
-// }
+  displayInfo() {
+    console.log(`Name: ${this.name}`);
+  }
+}
 
-// class Manager extends Employee {
-//   constructor(name, department) {
-//     super(name);
-//     this.department = department;
-//   }
+class Manager extends Employee {
+  constructor(name, department) {
+    super(name);
+    this.department = department;
+  }
 
-//   displayInfo() {
-//     console.log(`Name: ${this.name}, Department: ${this.department}`);
-//   }
-// }
+  displayInfo() {
+    console.log(`Name: ${this.name}, Department: ${this.department}`);
+  }
+}
 
-// const employee = new Employee("John Smith");
-// employee.displayInfo();
+const employee = new Employee("John Smith");
+employee.displayInfo();
 
-// const manager = new Manager("Jane Doe", "Sales");
-// manager.displayInfo();
+const manager = new Manager("Jane Doe", "Sales");
+manager.displayInfo();
 
 // Задание 2: "Управление списком заказов"
 
